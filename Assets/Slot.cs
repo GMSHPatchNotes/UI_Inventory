209c5708b6dca084b83575f7613e72c987d8ac3e
GMSHPatchNotes <127327029+GMSHPatchNotes@users.noreply.github.com>
2023-10-17 19:19:13 +0900

    Add files via upload

---
+++ Assets/Slot.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Slot : MonoBehaviour, IDropHandler
+{
+    GameObject Icon()
+    {
+        if(transform.childCount > 0)
+            return transform.GetChild(0).gameObject;
+        else
+            return null;
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if(Icon() == null)
+        {
+            IconDrag.beingDraggedIcon.transform.SetParent(transform);
+            IconDrag.beingDraggedIcon.transform.position = transform.position;
+        }
+    }
+}
